c6f469669b8bcfe4242435b861b454633348ab63
mackele <larsmarcus.lennartsson@gmail.com>
2023-05-17 13:34:16 +0200

    Show Score result after all questions have been answered

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,70 +1,65 @@
-import React from 'react'
-import Score from "./Score"
-
+import React, { useState } from 'react';
+import Score from "./Score";
 
 export default function Question(props) {
-  let {questions, questionIndex, score, setScore, setCurrentQuestionIndex, selectedCategoryName} = props;
+  const { questions, questionIndex, score, setScore, setCurrentQuestionIndex, selectedCategoryName } = props;
+  const [finished, setFinished] = useState(false);
 
-  function handleAnswerClick (event, isCorrect) {
-    
+  function handleAnswerClick(event, isCorrect) {
     if (isCorrect) {
       setScore(score + 1);
       event.currentTarget.classList.add("correct-answer");
     } else {
       event.currentTarget.classList.add("incorrect-answer");
-    };
-  };
+    }
+  }
 
-
-  function nextQuestion () {
-    let nextQuestionIndex = questionIndex + 1;
+  function nextQuestion() {
+    const nextQuestionIndex = questionIndex + 1;
     if (nextQuestionIndex < questions.length) {
       setCurrentQuestionIndex(nextQuestionIndex);
     } else {
-        const results = localStorage.getItem("Results");
-        let resultsArr = [];
-        if (results) {
-            resultsArr = JSON.parse(results);
-        };
-        let newScore = {
-            "Score": score,
-            "Category": selectedCategoryName
-        };
-        resultsArr.push(newScore);
-
+      const results = localStorage.getItem("Results");
+      let resultsArr = [];
+      if (results) {
+        resultsArr = JSON.parse(results);
+      }
+      const newScore = {
+        "Score": score,
+        "Category": selectedCategoryName
+      };
+      resultsArr.push(newScore);
       localStorage.setItem("Results", JSON.stringify(resultsArr));
-    };
-    console.log(questionIndex +1)
-    console.log(questions.length)
-  };
 
+      setFinished(true);
+    }
+  }
 
   return (
-    <div>Question component
-      {/* Visar upp den fråga man är på */}
+    <div>
       <h2>Question no: {questionIndex + 1} / {questions.length}</h2>
-      {/* Visar upp själva frågan */}
-      <h3>{questions[questionIndex].question}</h3>
+      <h3>{questions[questionIndex]?.question}</h3>
       <ul>
-      {[...questions[questionIndex].incorrect_answers, questions[questionIndex].correct_answer]
-        .sort(() => Math.random() - 0.5)
-        .map((answer) => (
-          <li
-            key={answer}
-            onClick={(event) => handleAnswerClick(event, answer === questions[questionIndex].correct_answer)}
-          >
-            {answer}
-          </li>
-        ))}
+        {questions[questionIndex]?.incorrect_answers && questions[questionIndex]?.correct_answer && [
+          ...questions[questionIndex].incorrect_answers,
+          questions[questionIndex].correct_answer
+        ]
+          .sort(() => Math.random() - 0.5)
+          .map((answer) => (
+            <li
+              key={answer}
+              onClick={(event) => handleAnswerClick(event, answer === questions[questionIndex]?.correct_answer)}
+            >
+              {answer}
+            </li>
+          ))}
       </ul>
-      <button onClick={nextQuestion}>Nästa fråga</button>
+      <button onClick={nextQuestion}>{questionIndex === questions.length - 1 ? "Finish" : "Next Question"}</button>
       <div className='score'>
         <p>Score:</p>
         <p>{score}</p>
       </div>
-      {questions.length +1 === questionIndex +2  && (
-        <Score index={questionIndex} score={score}/>
-      )}
+      {finished && <Score index={questionIndex} score={score} />}
     </div>
   );
-};
+}
