268065fb7776f0313799aceb6f44aa8a25bd17c6
mackele <larsmarcus.lennartsson@gmail.com>
2023-05-17 14:00:57 +0200

    Return to start after quiz is finished
    Click on Meny which runs reload function

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,13 +1,10 @@
-import Game from "./Game"
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Score (props){
-    const [returnToMenu, setReturn] = useState(false);
     const { index, score } = props;
 
     function reload(){
-      setReturn(true)
-
+        window.location.reload();
     }
 
     return (
@@ -15,7 +12,6 @@ export default function Score (props){
             <h3>Ditt Resultat</h3>
             <p>{score}/{index}</p>
             <button onClick={reload}>Meny</button>
-            {returnToMenu && <Game />}
         </div>
     )
 
