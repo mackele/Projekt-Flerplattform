e2a96f9193c4ff5b1f2b81b1862e4052c3cdea80
mackele <larsmarcus.lennartsson@gmail.com>
2023-05-17 11:34:50 +0200

    Added component for question
    Functionality for question component, show and let user choose between different options

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,23 +1,49 @@
 
 import React, {useState} from 'react'
 import Menu from './Menu';
+import { fetchQuestionsByCategory } from '../utils/trivia';
+import Question from './Question';
 
 
 export default function Game() {
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedCategoryName, setSelectedCategoryName] = useState(null);
-  const [numQuestions, setNumQuestions] = useState(1);
+  const [numQuestions, setNumQuestions] = useState(5);
   const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0)
+  const [showScore, setShowScore] = useState(false);
+
+  async function handleStartQuiz () {
+    const quizQuestions = await fetchQuestionsByCategory(
+      numQuestions, selectedCategory
+    );
+    setCurrentQuestionIndex(0);
+    setQuestions(quizQuestions);
+    setScore(0);
+    console.log("Quizen startar")
+  }
   
 
   return (
-    <div>Game component
-      <Menu categories={categories} setCategories={setCategories}
-      selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}
-      selectedCategoryName={selectedCategoryName} setSelectedCategoryName={setSelectedCategoryName}
-      numQuestions={numQuestions} setNumQuestions={setNumQuestions}
-      questions={questions} setQuestions={setQuestions}/>
+    <div id="container">
+      <h1>Hejsan</h1>
+      {!questions.length && (
+        <div id="menu">
+          Game component - TA BORT SEN
+          <Menu categories={categories} setCategories={setCategories}
+          selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}
+          selectedCategoryName={selectedCategoryName} setSelectedCategoryName={setSelectedCategoryName}
+          numQuestions={numQuestions} setNumQuestions={setNumQuestions}
+          questions={questions} setQuestions={setQuestions}/>
+
+          <button onClick={handleStartQuiz} disabled={!selectedCategory}>Starta denna fantastiska quiz</button> 
+        </div>
+      )}
+      {questions.length > 0 && (
+        <Question questions={questions} questionIndex={currentQuestionIndex} score={score} setScore={setScore} setCurrentQuestionIndex={setCurrentQuestionIndex} selectedCategoryName={selectedCategoryName} showScore={showScore}></Question>
+      )}
     </div>
   );
 };
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,8 +2,59 @@
 import React from 'react'
 
 
-export default function Question() {
+export default function Question(props) {
+  const {questions, questionIndex, score, setScore, setCurrentQuestionIndex, currentQuestionIndex, selectedCategoryName} = props;
+
+  const handleAnswerClick = (event, isCorrect) => {
+    const target = event.currentTarget;
+    if (isCorrect) {
+      setScore(score + 1);
+      target.classList.add("correct-answer");
+    } else {
+      target.classList.add("incorrect-answer");
+    }
+  }
+
+  const nextQuestion = () => {
+    const nextQuestionIndex = questionIndex + 1;
+    if (nextQuestionIndex < questions.length) {
+      setCurrentQuestionIndex(nextQuestionIndex);
+    } else {
+        const results = localStorage.getItem("Results");
+        let resultsArr = [];
+        if (results) {
+            resultsArr = JSON.parse(results);
+        };
+        let newScore = {
+            "Score": score,
+            "Category": selectedCategoryName
+        };
+        resultsArr.push(newScore);
+
+      localStorage.setItem("Results", JSON.stringify(resultsArr));
+    };
+  };
+  
+  console.log(questions);
   return (
-    <div>Question component</div>
+    <div>Question component
+      {/* Visar upp den fråga man är på */}
+      <h2>Question no: {questionIndex + 1} / {questions.length}</h2>
+      {/* Visar upp själva frågan */}
+      <h3>{questions[questionIndex].question}</h3>
+      <ul>
+      {[...questions[questionIndex].incorrect_answers, questions[questionIndex].correct_answer]
+        .sort(() => Math.random() - 0.5)
+        .map((answer) => (
+          <li
+            key={answer}
+            onClick={(event) => handleAnswerClick(event, answer === questions[questionIndex].correct_answer)}
+          >
+            {answer}
+          </li>
+        ))}
+      </ul>
+      <button onClick={nextQuestion}>Nästa fråga</button>
+    </div>
   );
 };
